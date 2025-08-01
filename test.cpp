d37794ad763f62178618911e29be15ac5b023640
CornOnTheCob <136200440+bluesiarra@users.noreply.github.com>
2025-07-31 17:30:59 -0700

    learned about references, pointers, dereferences, null pointers, etc

--- test.cpp
+++ test.cpp
@@ -2,6 +2,34 @@
 using namespace std;
 
 int main() {
-  cout << "Hello, world!";
-  return 0;
+    //cout << "Hello, world!";
+
+//references
+    int number = 85;
+    int& ref_num = number; //references number
+    //must be initalized
+    //cannot be reassigned
+
+
+    //memory address
+    string msg = "Hello world!";
+
+    cout << msg << endl;
+    cout << &msg << endl; //prints the address of the string, not the actual string
+
+    //pointer
+    //stores memory address of something
+
+    int testValue = 67;
+    int* testValueAddress = &testValue;
+
+    cout << testValueAddress << endl; //prints out the address
+    cout << *testValueAddress << endl; //prints out the value
+
+    *testValueAddress = 420; //changes the actual variable
+
+    //pointers can be null
+    int* pointer = nullptr;
+
+    return 0;
 }
